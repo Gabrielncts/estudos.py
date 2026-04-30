b6f6991f9afbe61b98e2563bc6afccf6fffc53cf
Gabriel "Noctis" Oliveira <47607193+Gabrielncts@users.noreply.github.com>
2026-04-30 20:08:20 -0300

    Update ex7Lista.py

--- ex7Lista.py
+++ ex7Lista.py
@@ -1,5 +1,11 @@
 print(" LOCALIZADOR DE VOGAIS ")
-palavra = input("digite uma palavra: ")
-vogais = 'a' , 'e', 'i', 'o', 'u'
-if vogais in palavra :
-    print("Contém vogais")
+palavra = input("digite uma palavra: ").lower()
+vogais = 'aeiou'
+#for usado para verificar letra por letra da palavra digitada;
+encontrou = False
+for letra in palavra :
+    if letra in vogais :
+        print(f"A vogal '{letra}' foi encontrada na palavra")
+        encontrou = True
+if not encontrou :
+    print("palavra não possui vogal")
